ca8e97a2983808d9b3e2e23aafb4298a0f84b1f5
Onion <onionfan@yandex.ru>
2017-01-29 22:12:12 +0300

    Added news - just test

--- index.android.js
+++ index.android.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import {AppRegistry,Text,StyleSheet,View} from 'react-native'
 import Rate from './src/rate.js'
 import Chart from './src/chart.js'
+import News from './src/news.js'
 
 export default class AwesomeProject extends Component {
   render() {
@@ -14,7 +15,7 @@ export default class AwesomeProject extends Component {
           flexDirection: 'column',
           justifyContent: 'space-between',}}>
           <Rate name={'btc-e'} />
-
+          <News />
     </View>
     <View style={{flex: 1, backgroundColor: 'lightcyan'}} />
     </View>
--- src/news.js
+++ src/news.js
@@ -3,8 +3,53 @@ import React, {
   Component,
 } from 'react'
 
-
 import {
   View,
   Text,
+  TouchableOpacity,
+  Linking
 } from 'react-native'
+
+export default class Bitcoin extends Component {
+
+constructor() {
+   super()
+   this.state = {
+      myText: 'Подождите',
+      myText2: 'Подождите'
+   }
+}
+getrate()
+{fetch('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fbitnovosti.com%2Ffeed%2F')
+  .then((response) => response.json())
+  .then((responseJson) => {
+  var average = responseJson.items[1].title
+  var average2 = responseJson.items[1].description
+  this.setState({myText: average})
+  this.setState({myText2: average2})
+  })}
+
+onPressButton(){
+Linking.openURL("https://facebook.github.io/react-native/docs/linking.html").catch(err => console.error('An error occurred', err));
+
+}
+
+  render() {
+     return (
+        <View>
+        {this.getrate()}
+        <TouchableOpacity onPress={() => this.onPressButton()}>
+         <Text style={{fontSize: 25}}>
+              {this.state.myText}
+         </Text>
+
+         <Text style={{fontSize: 15}}>
+              {this.state.myText2}
+         </Text>
+         </TouchableOpacity>
+        </View>
+     );
+  }
+
+
+}
