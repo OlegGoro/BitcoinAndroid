b5b90c99912394701e2e21ef25a2439d068a4aa5
funri <2641387@gmail.com>
2017-01-29 13:52:31 +0300

    Style fix

--- index.android.js
+++ index.android.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
-import {AppRegistry,Text,StyleSheet,View} from 'react-native'
+import {AppRegistry,StyleSheet,Text,View} from 'react-native'
 import Rate from './src/rate.js'
 import Chart from './src/chart.js'
 
 export default class AwesomeProject extends Component {
   render() {
     return (
-    <View style={{flex: 1}}>
+    <View style={{flex: 2,justifyContent: 'center',
+        alignItems: 'center'}}>
+    <Text> Bitcoin: </Text>
+    <View style={{flex:1, backgroundColor: 'lightgrey'}} />
     <View style={{
           flexDirection: 'column',
+          alignItems: 'center',
           justifyContent: 'space-between',}}>
           <Rate name={'btc-e'} />
-    <View style={{flex:1, backgroundColor: 'lightgrey'}} />
-
-
 
     </View>
         <Chart />
